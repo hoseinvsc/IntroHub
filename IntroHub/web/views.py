c6f08ca491cb51b9345e97f6687263d4549dafee
hoseinvsc <hoseinvsc@gmail.com>
2025-02-14 15:44:26 +0330

    creat primary views!

--- IntroHub/web/views.py
+++ IntroHub/web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def connect(request):
+    return render(request, 'connect.html')
+
+def product(request):
+    return render(request, 'product.html')
