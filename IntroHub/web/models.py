b1c7182d72fca561b31c0d55a5c53f51a9d33c53
kazem3d <kazem3d@gmail.com>
2025-02-20 12:59:35 +0330

    add ProductImage and Category models

--- IntroHub/web/models.py
+++ IntroHub/web/models.py
@@ -3,10 +3,20 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100,verbose_name="نام محصول")
     description = models.TextField(verbose_name="توضیحات")
-    
+    category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True,blank=True)
+
     class Meta:
         verbose_name = "محصول"
         verbose_name_plural = "محصولات"
     
     def __str__(self):
         return self.name
+    
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='images')
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=24)
