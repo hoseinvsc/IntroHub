6c24a1c8eaef481f21e6d8057480db0ed30a4b6d
hoseinvsc <hoseinvsc@gmail.com>
2025-02-20 12:22:59 +0330

    ----

--- IntroHub/web/views.py
+++ IntroHub/web/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products,}
+    return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html')
